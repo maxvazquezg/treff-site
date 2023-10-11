a5b9f0d6429773683a2a68ef80bdc2c90d19a068
Max Vazquez <maxvazquezg@hotmail.com>
2023-10-11 13:44:24 -0600

    add new spinner loading

--- src/LoadingIndicator.js
+++ src/LoadingIndicator.js
@@ -1,20 +1,24 @@
 import { usePromiseTracker } from "react-promise-tracker";
 // import Loader from 'react-loader-spinner';
 import {BallTriangle} from 'react-loader-spinner';
+import { getURLImage } from "./utils/images";
 // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const LoadingIndicator = props => {
     const { promiseInProgress } = usePromiseTracker();
     return (
         promiseInProgress &&
-        <div className="overlay"><BallTriangle
+        <div className="overlay">
+            {/* <BallTriangle
             type="TailSpin"
             color="#FFF"
             background="rgba(63, 81, 181, 0.7)"
             fullscreen={true}
             dismissible={true}
             show={true}
-        /></div>
+        /> */}
+        <img src={getURLImage("images/spinner2.gif", true)} width={100} alt="loading" />
+        </div>
     );
 }
 

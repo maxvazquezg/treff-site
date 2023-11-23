34ba4bccf27350cca9c2b20d32b3b559c434091e
Max Vazquez <maxvazquezg@hotmail.com>
2023-11-22 18:44:55 -0600

    fix about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -8,11 +8,21 @@ const AboutUs = () => {
         <h1>Sobre TREFF</h1>
         <br />
         <p>
+          En el corazón de nuestra identidad, "TREFF" se inspira en la palabra
+          alemana que significa "encuentro" o "reunión". Somos más que una
+          plataforma; somos un punto de encuentro virtual creado por
+          profesionales y freelancers para freelancers. En TREFF, creemos en
+          canalizar el talento de las personas y conectarlas con clientes
+          exigentes, estableciendo un espacio donde las habilidades se
+          encuentran con las necesidades.
+        </p>
+
+        <p>
           TREFF es una empresa mexicana con sede en Guadalajara, Jalisco,
           fundada por profesionales y freelancers, con un enfoque específico en
           atender las necesidades de otros freelancers.
         </p>
-        <br />
+
         <p>
           Contamos con una sólida experiencia en áreas como administración,
           contabilidad, finanzas, asesoría fiscal, desarrollo y gestión de
@@ -21,7 +31,6 @@ const AboutUs = () => {
           personas para satisfacer las demandas de los clientes más exigentes.
         </p>
 
-        <br />
         <p>
           Actuamos como intermediarios entre freelancers y el mercado, siempre
           asegurándonos de que se entreguen trabajos de alta calidad a clientes
@@ -29,9 +38,7 @@ const AboutUs = () => {
           partes.
         </p>
 
-        <br />
         <p>Para ti, cliente:</p>
-        <br />
 
         <p>
           En TREFF, te ofrecemos una plataforma donde encontrarás una amplia
@@ -41,7 +48,6 @@ const AboutUs = () => {
           personalizado. En TREFF, actuamos como mediadores entre tus
           necesidades y la entrega de un trabajo de calidad.
         </p>
-        <br />
 
         <p>
           Nuestro catálogo de servicios abarca una amplia gama, ofrecidos por
@@ -49,7 +55,6 @@ const AboutUs = () => {
           realice actividades fuera de tu área de experiencia de manera remota,
           aprovechando tecnologías digitales para resolverlo.
         </p>
-        <br />
 
         <p>
           Nos comprometemos a cuidar de tu inversión y garantizar la calidad del
@@ -60,17 +65,14 @@ const AboutUs = () => {
           seguridad en la contratación, tu inversión y la calidad del servicio
           que recibirás.
         </p>
-        <br />
 
         <p>Para ti, freelancer:</p>
-        <br />
 
         <p>
-          En TREFF, ofrecemos una herramienta confiable diseñada para que
-          ofrezcas tus servicios como experto independiente de manera amigable,
-          ágil y segura.
+          Ofrecemos una herramienta confiable diseñada para que ofrezcas tus
+          servicios como experto independiente de manera amigable, ágil y
+          segura.
         </p>
-        <br />
 
         <p>
           Siempre contarás con el respaldo de TREFF, asegurando tus pagos por
@@ -78,20 +80,19 @@ const AboutUs = () => {
           tarifas. Dejamos en tus manos la posibilidad de aumentar y exponer tu
           talento según tus preferencias.
         </p>
-        <br />
 
         <p>
-          Comprometidos con tu éxito, nos encargamos de posicionar y promocionar
-          tus servicios en un mercado que abarca clientes potenciales en México
-          y Latinoamérica.
+          En TREFF, estamos comprometidos a ser el punto de encuentro que
+          posiciona y promociona tus servicios en un mercado que abarca clientes
+          potenciales en México y Latinoamérica. Creemos en la calidad, la
+          seguridad y la validación, garantizando que todos los freelancers en
+          nuestra comunidad hayan pasado por un riguroso proceso de confirmación
+          de perfil.
         </p>
-        <br />
 
         <p>
-          (*) Garantizamos que todos los freelancers en la comunidad TREFF han
-          pasado previamente por un proceso de validación y confirmación de
-          perfil, pensando en la seguridad de tu contratación, en tu inversión y
-          en la calidad del servicio que recibirás.
+          Bienvenido a TREFF, donde el talento se encuentra con las
+          oportunidades, y cada encuentro es una puerta abierta hacia el éxito.
         </p>
       </div>
     </CustomSection>
